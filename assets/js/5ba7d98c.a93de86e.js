fddf482609eef4d72c7e6c642b2858b7ebd720a5
Fede-Berga <Fede-Berga@users.noreply.github.com>
2025-06-11 09:13:28 +0000

    deploy: 93918b86220ae0e08d909326489deb35b88af20f

--- assets/js/5ba7d98c.a93de86e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpug=self.webpackChunkpug||[]).push([[100],{3160:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/PUG/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.9c4c9e72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpug=self.webpackChunkpug||[]).push([[284],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
